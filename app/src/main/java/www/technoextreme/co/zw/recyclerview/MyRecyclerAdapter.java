2490c6bc2f642302d4d3604aed6b8dbb575e0d7c
marlonmilesmg <magonjomarlon@gmail.com>
2020-05-21 16:56:27 +0200

    Generic

---
+++ app/src/main/java/www/technoextreme/co/zw/recyclerview/MyRecyclerAdapter.java
@@ -0,0 +1,25 @@
+package www.technoextreme.co.zw.recyclerview;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyRecyclerAdapter extends RecyclerView.Adapter<MyHolder> {
+
+    @NonNull
+    @Override
+    public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
